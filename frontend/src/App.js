2278c1539087105c1f4f157e3409ac6202e3a83e
santileira <santiago.leira@mercadolibre.co>
2019-08-15 16:54:17 -0300

    designing out fe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,59 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./App.css";
-import { connect, sendMsg } from "./index";
+import {connect, sendMsg} from "./index";
+import Header from './components/Header/Header';
+import ChatHistory from './components/ChatHistory/ChatHistory';
+
+const URL = "ws://localhost:8080/ws";
 
 class App extends Component {
     constructor(props) {
         super(props);
+        this.state = {chatHistory: []}
     }
 
-    send() {
-        console.log("hello");
-        sendMsg("hello");
+    ws = new WebSocket(URL);
+
+    componentDidMount() {
+        this.ws.onopen = () => {
+            console.log("Successfully Connected");
+        };
+        this.ws.onmessage = msg => {
+            console.log(msg);
+            this.addMessage(msg);
+        };
+
+        this.ws.onclose = event => {
+            console.log("Socket Closed Connection: ", event);
+        };
+
+        this.ws.onerror = error => {
+            console.log("Socket Error: ", error);
+        };
     }
+
+    addMessage = message => {
+        this.setState(prevState => ({
+            chatHistory: [...this.state.chatHistory, message]
+        }))
+    };
+
+    submitMessage = messageString => {
+        this.ws.send(messageString);
+    };
 
     render() {
+
         return (
             <div className="App">
-                <button onClick={this.send}>Hit</button>
+                <Header/>
+                <ChatHistory chatHistory={this.state.chatHistory}/>
+                <button onClick={this.submitMessage}>Hit</button>
             </div>
         );
     }
+
+
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,32 +10,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-var socket = new WebSocket("ws://localhost:8080/ws");
-
-let connect = () => {
-    console.log("Attempting Connection...");
-
-    socket.onopen = () => {
-        console.log("Successfully Connected");
-    };
-
-    socket.onmessage = msg => {
-        console.log(msg);
-    };
-
-    socket.onclose = event => {
-        console.log("Socket Closed Connection: ", event);
-    };
-
-    socket.onerror = error => {
-        console.log("Socket Error: ", error);
-    };
-};
-
-let sendMsg = msg => {
-    console.log("sending msg: ", msg);
-    socket.send(msg);
-};
-
-export { connect, sendMsg };
