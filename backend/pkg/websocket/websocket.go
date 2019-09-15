1189a05ffeed41543b2245c4cf81e71ce0cc45d9
sleira <sleira@medallia.com>
2019-09-15 17:26:34 -0300

    finish handling multiple clients

--- backend/pkg/websocket/websocket.go
+++ backend/pkg/websocket/websocket.go
@@ -10,9 +10,10 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool { return true },
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
+// Upgrade upgrades connection to the Websocket protocol and returns a new connection
 func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
